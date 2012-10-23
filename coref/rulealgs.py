3d7348cff93a519620f0fcd1da352ff4bc4d66f0
Adam Walz <adamwalz@me.com>
2012-10-22 19:18:16 -0600

    Initial commit of rule based algorithms

---
+++ coref/rulealgs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+    coref.rulealgs
+    ~~~~~~~~~~~~
+
+    Algorithms for rule based for coreference resolution
+
+    :copyright: (c) 2012 by Adam Walz, Charmaine Keck
+    :license: 
+"""
+
+def string_matcher(fileparse):
+    pass
+
+def hobbs(fileparse, coref_id):
+    pass
+    
